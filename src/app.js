f3f57d8eac16b37fc600faffb72bf5542e764337
matincodes <abdulmatinadeniji@gmail.com>
2024-01-09 08:41:19 +0100

    adding new files

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const companiesRouter = require('./routes/companies/companies.router')
 
 const app = express()
 
-app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors({ origin: 'https://info-africa.vercel.app' }))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
